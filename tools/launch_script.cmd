28ee1b2ca7b533c6608cec2b4bc907ad0e1c2a9c
jmor2000 <morganje@tcd.ie>
2022-07-22 11:32:15 +0100

    tools del

--- tools/launch_script.cmd
+++
@@ -1,8 +0,0 @@
-@echo off 
-echo %1 
-echo %2 
-echo %3
-echo %4
-echo %5
-echo %6
-PowerShell.exe -WindowStyle Hidden -Command py '%1' -m '%2' -n '%3' -g '%4' -l %5 -r %6
--- tools/launch_uno.py
+++
@@ -1,4 +0,0 @@
-import os 
-
-os.system("""start cmd /k PowerShell.exe -WindowStyle Normal -Command py scripts/rapper.py -m "script_display.myscript" -n ""app_display" -g "group1\group2\group3" -l False -r False """)
-os.system("""start cmd /k PowerShell.exe -WindowStyle Normal -Command py scripts/rapper.py -m "script_Helloworld.myscript" -n ""app_HelloWorld" -g "group1\group2\group3" -l False -r False """)
